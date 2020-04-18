35a05cf8f50e04392e8e488e47c0f47e082c4b48
kourouma abdoulye <kouroumaa@yahoo.fr>
2020-04-18 15:17:09 +0200

    First commit

---
+++ src/main/java/com/example/devopsstep1/DevopsStep1Application.java
@@ -0,0 +1,13 @@
+package com.example.devopsstep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsStep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsStep1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsstep1/DevopsStep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsstep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsStep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
