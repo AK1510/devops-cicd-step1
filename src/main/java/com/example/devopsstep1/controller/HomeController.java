56623cfb740da278427116ed49c0ea157ff0ace1
kourouma abdoulye <kouroumaa@yahoo.fr>
2020-04-18 15:29:17 +0200

    Adding HomeController to the project

--- src/main/java/com/example/devopsstep1/controller/HomeController.java
+++ src/main/java/com/example/devopsstep1/controller/HomeController.java
@@ -1,8 +1,16 @@
 package com.example.devopsstep1.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+
+    @GetMapping("/hi")
+    public String sayHi(){
+
+        return  "hello";
+    }
 }
